e2a4238c7b21d859783fbd4aae541d9a0669b683
hareleps <harel.eps@gmail.com>
2020-05-24 21:40:10 +0300

    my little help 24-5

--- BullsEye/BullsEye/GameWindow.xaml.cs
+++ BullsEye/BullsEye/GameWindow.xaml.cs
@@ -19,15 +19,18 @@ namespace BullsEye
     /// </summary>
     public partial class GameWindow : Window
     {
+        int currRaw = 0;
+
         public GameWindow()
         {
             InitializeComponent();
             BuildFirstRaw();
-             Buttons();
+            Buttons();
         }
 
         private void Buttons()
         {
+            // שימו לב שפעולה זו לא עושה כלום. אתם תסבירו לי למה
             StackPanel myVertical = new StackPanel();
             myVertical.Orientation = Orientation.Vertical;
             for (int i = 0; i < 6; i++)
@@ -37,27 +40,96 @@ namespace BullsEye
                 color.Content = color.Name;
                 color.Margin = new Thickness(20);
                 myVertical.Children.Add(color);
-            }
-            
-            
+            }    
         }
 
         private void BuildFirstRaw()
         {
             StackPanel myRaw = new StackPanel();
             myRaw.Orientation = Orientation.Horizontal;
+            myRaw.Background = Brushes.Black;
             for (int i = 0; i < 4; i++)
             {
-                Button bt = new Button();
-                bt.Name = "bt" + (i+1);
-                bt.Content = bt.Name;
-                bt.Margin = new Thickness(5);
-                bt.Background = Brushes.Aqua;
-                myRaw.Children.Add(bt);
+                Ellipse eli = new Ellipse();
+                eli.Name = "eli" + (i+1);
+                eli.Width = 30;
+                eli.Height = 30;
+                eli.MouseDown += ChoseColorClick;
+                eli.Fill = Brushes.Gray;
+                eli.Margin = new Thickness(10);
+                myRaw.Children.Add(eli);
             }
+            currRaw = 0;
+
+            Button checkIt = CreateCheckItButton();
+            Button clearRaw = CreateClearButton();
+
+            myRaw.Children.Add(checkIt);
+            myRaw.Children.Add(clearRaw);
             MyBoard.Children.Add(myRaw);
         }
 
-       
+        private Button CreateClearButton()
+        {
+            Button clear = new Button();
+            // create your button
+            return clear;
+        }
+
+        private Button CreateCheckItButton()
+        {
+            Button checkIt = new Button();
+            checkIt.Width = 100;
+            checkIt.Height = 30;
+            checkIt.Margin = new Thickness(30, 5, 0, 0);
+            checkIt.Background = Brushes.White;
+            checkIt.Content = "Check";
+            checkIt.FontSize = 20;
+            checkIt.Foreground = Brushes.Black;
+            checkIt.FontWeight = FontWeights.Bold;
+            checkIt.Click += CheckIt_Click;
+            return checkIt;
+        }
+
+        
+        private void CheckIt_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ChoseColorClick(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void Yellow_Click(object sender, RoutedEventArgs e)
+        {
+            StackPanel s = (StackPanel)MyBoard.Children[currRaw];
+            for (int i = 0; i < 4; i++)
+            {
+                Ellipse el = (Ellipse)s.Children[i];
+                if (el.Fill == Brushes.Gray)
+                {
+                    el.Fill = Brushes.Yellow;
+                    Yellow.IsEnabled = false;
+                    break;
+                }
+            }
+        }
+
+        private void Green_Click(object sender, RoutedEventArgs e)
+        {
+            StackPanel s = (StackPanel)MyBoard.Children[currRaw];
+            for (int i = 0; i < 4; i++)
+            {
+                Ellipse el = (Ellipse)s.Children[i];
+                if (el.Fill == Brushes.Gray)
+                {
+                    el.Fill = Brushes.Green;
+                    Green.IsEnabled = false;
+                    break;
+                }
+            }
+        }
     }
 }
